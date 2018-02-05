46c35580fe806ad6ee99596512388fa85ca1e84f
Jonah Meijers <jonahmeijers97@gmail.com>
2018-02-05 22:58:23 +0100

    forgot to require

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv').config()
 const webpack = require('webpack')
+const Dotenv = require('dotenv-webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
